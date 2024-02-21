527921a354afb795d35cbbaf09822090ef3ec0c7
Yaminimini19 <160703203+Yaminimini19@users.noreply.github.com>
2024-02-21 11:22:11 +0530

    Add files via upload

---
+++ flames.py
@@ -0,0 +1,33 @@
+import streamlit as st
+st.title("welcome to the game")
+a=st.text_input("enter first name-")
+b=st.text_input("enter second name-")
+a=list(a)
+b=list(b)
+count=0
+for i in range(len(a)):
+    for j in range(len(b)):
+        if a[i]==b[j]:
+            a[i]='0'
+            b[j]='0'
+            break
+count=0
+for i in a+b:
+    if i!=0:
+        count+=1
+res=["Friends","Lovers","Attraction","Marriage","Enemies","Siblings"]
+while len(res)>1:
+    splitIndex=(count%len(res)-1)
+    if splitIndex>=0:
+        right=res[splitIndex+1:]
+        left=res[:splitIndex]
+        res=right+left
+    else:
+        res=res[:len(res)-1]
+print(res)
+if st.button('submit'):
+    st.success(res[0])
+
+
+
+
